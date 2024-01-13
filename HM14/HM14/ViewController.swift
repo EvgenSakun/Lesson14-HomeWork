69f490884b2672cdbd96435d9dc1e4c205e3c6f6
EvgenSakun <evgen.sakun20@gmail.com>
2024-01-13 22:49:12 +0300

    start

---
+++ HM14/HM14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HM14
+//
+//  Created by Evgeny Sakun on 13.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
